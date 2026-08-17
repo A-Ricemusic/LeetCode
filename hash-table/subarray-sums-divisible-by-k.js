730e1946866850aa382cf394e90ecbe9d27f75a7
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-08-17 15:57:59 -0700

    LC-subarray-sums-divisible-by-k [Runtime: N/A; Memory: N/A]

---
+++ hash-table/subarray-sums-divisible-by-k.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraysDivByK = function(nums, k) {
+    let hashMap = new Map();
+    let total = 0;
+    let res = 0;
+    hashMap.set(0,1)
+
+    for (const num of nums) {
+        total += num
+        const remain = total % k;
+        if (remain < 0) remain += k;
+        res += hashMap.get(remain) || 0
+        hashMap.set(remain, (hashMap.get(remain) || 0) + 1) 
+    }
+
+    return res
+    
+};
