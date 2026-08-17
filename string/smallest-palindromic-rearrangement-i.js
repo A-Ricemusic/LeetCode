8c9bfa9d9659e08e9fbdcf95879e9e4564bff6a9
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-08-17 13:21:07 -0700

    LC-smallest-palindromic-rearrangement-i [Runtime: N/A; Memory: N/A]

--- string/smallest-palindromic-rearrangement-i.js
+++ string/smallest-palindromic-rearrangement-i.js
@@ -1,6 +1,8 @@
 /**
  * @param {string} s
  * @return {string}
+
+    "dacacad" -> "
  */
 var smallestPalindrome = function(s) {
     let l = 1;
@@ -11,19 +13,13 @@ var smallestPalindrome = function(s) {
         let j = r
         
         while (i > 0 && s[i].charCodeAt(0) < s[i - 1].charCodeAt(0)) {
-            let tmp = s[i]
-            s[i] = s[i - 1]
-            s[i - 1] = tmp
-            tmp = s[j]
-            s[j] = s[j + 1]
-            s[j + 1] = tmp
+            [s[i], s[i - 1]] = [s[i - 1], s[i]];
+            [s[j], s[j + 1]] = [s[j + 1], s[j]];  
             i--;
             j++;
         }
         l++;
         r--;
     }
-
     return s.join("")
-    
 };
