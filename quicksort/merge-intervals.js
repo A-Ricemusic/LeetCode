9d23944cd1b4ff3adccbe121a2189e0658abc0d2
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-08-18 11:17:12 -0700

    LC-merge-intervals [Runtime: N/A; Memory: N/A]

--- quicksort/merge-intervals.js
+++ quicksort/merge-intervals.js
@@ -8,14 +8,13 @@ var merge = function(intervals) {
     res.push(intervals[0])
     for (let i = 1; i < intervals.length; i++) {
         const start = intervals[i][0]
-        const end = intervals[i][1]
+        const end1 = intervals[i][1]
         const end2 = res[res.length - 1][1]
-        if (start <= end2) {
-            if (end > end2) {
-                res[res.length - 1][1] = end
-            }
+        if (start <= end2 && end1 > end2) {
+            res[res.length - 1][1] = end1
             
-        } else {
+        }
+        if (start > end2) {
             res.push(intervals[i])
         }
     }
