0eaa9d6a78f8c3397ad93e3e5aa02705e0821fb3
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-08-20 15:47:14 -0700

    LC-sort-an-array [Runtime: 155 ms; Memory: 75.7 MB]

--- merge-sort/sort-an-array.js
+++ merge-sort/sort-an-array.js
@@ -3,27 +3,57 @@
  * @return {number[]}
  */
 var sortArray = function(nums) {
-    quickSort(nums,0, nums.length - 1)
-    return nums
+    return mergeSort(nums,0, nums.length - 1)
     
 };
 
+/* 
+
+ nums = [5,2,3,1]
+     [5,2]. [3, 1]
+    [5] [2] [3] [1]
 
 
-var quickSort = function(nums,s,e) {
-    if (e - s + 1 <= 1) return;
-    const pivot = e
-    let l = s
-    for (let i = s; i < e; i++) {
-        if (nums[i] <= nums[pivot]) {
-            [nums[i], nums[l]] = [nums[l], nums[i]];
-            l++
+*/
+
+const mergeSort = function(nums,s,e) {
+        if (e - s + 1 <= 1) return nums;
+        const m = Math.floor(s + (e - s) / 2)
+        mergeSort(nums,s,m)
+        mergeSort(nums,m + 1, e)
+        merge(nums,s,m,e)
+        return nums
+}
+
+
+const merge = function(nums,s,m,e) {
+    const L = nums.slice(s, m + 1);
+    const R = nums.slice(m + 1, e + 1);
+    let i = 0;
+    let j = 0;
+    let k = s;
+
+    while (i < L.length && j < R.length) {
+        if (L[i] < R[j]) {
+            nums[k] = L[i];
+            i++;
+        } else {
+            nums[k] = R[j];
+            j++;
         }
-
+        k++;
     }
-    [nums[pivot], nums[l]] = [nums[l], nums[pivot]]
 
-    quickSort(nums, s , l - 1)
-    quickSort(nums, l + 1, e)
 
-} 
+    while (i < L.length) {
+        nums[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < R.length) {
+        nums[k] = R[j];
+        j++;
+        k++;
+    }
+}
