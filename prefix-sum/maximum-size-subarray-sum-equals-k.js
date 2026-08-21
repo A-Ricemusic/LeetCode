f700987b0555b24f9bf06f1b21918d49a47063ad
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-08-20 17:39:43 -0700

    LC-maximum-size-subarray-sum-equals-k [Runtime: 72 ms; Memory: 93.9 MB]

--- prefix-sum/maximum-size-subarray-sum-equals-k.js
+++ prefix-sum/maximum-size-subarray-sum-equals-k.js
@@ -2,26 +2,25 @@
  * @param {number[]} nums
  * @param {number} k
  * @return {number}
-
  */
 var maxSubArrayLen = function(nums, k) {
-    let res = 0
     const hashMap = new Map()
     let total = 0
-
+    let res = 0
     for (let i = 0; i < nums.length; i++) {
         total += nums[i]
         if (total === k) {
-            res = i + 1
+            res = i + 1;
         }
-        if (hashMap.has(total - k)) {
-            res = Math.max(res, i - hashMap.get(total - k))
+        const key = total - k
+        if (hashMap.has(key)) {
+            res = Math.max(res, i - hashMap.get(key))
         }
         if (!hashMap.has(total)) {
             hashMap.set(total, i)
         }
-    }
 
+    }
 
     return res
     
