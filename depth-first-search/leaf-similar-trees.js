6f15347cd1db7f9cd9748203471a89f7f52eedf7
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-08-24 21:50:24 -0700

    LC-leaf-similar-trees [Runtime: N/A; Memory: N/A]

---
+++ depth-first-search/leaf-similar-trees.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+var leafSimilar = function(root1, root2) {
+    const arr1 = []
+    const arr2 = []
+
+    const dfs = (root, arr) => {
+        if (!root.left && !root.right) {
+            arr.append(root.val)
+        }
+        dfs(root.left, arr)
+        dfs(root.right,arr)
+    }
+    dfs(root1,arr1)
+    dfs(root2,arr2)
+    if (arr1.length !== arr2.length) return false;
+
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) return false;
+    }
+
+    return true;    
+};
