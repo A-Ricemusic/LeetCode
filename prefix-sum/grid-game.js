267cfafc27c3c84adec71c8565c193bdb4b33cd3
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-08-28 10:59:06 -0700

    LC-grid-game [Runtime: N/A; Memory: N/A]

--- prefix-sum/grid-game.js
+++ prefix-sum/grid-game.js
@@ -4,22 +4,21 @@
  */
 var gridGame = function(grid) {
     const N = grid[0].length;
-    let preRow1 = [...grid[0]];
-    let preRow2 = [...grid[1]];
+    const preRow1 = [...grid[0]];
+    const preRow2 = [...grid[1]];
 
     for (let i = 1; i < N; i++) {
         preRow1[i] += preRow1[i - 1];
-        preRow2[i] += preRow2[i - 1];
-    };
-
+        preRow2[i] += preRow2[i - 1]; 
+    }
     let res = Infinity
     for (let i = 0; i < N; i++) {
         const top = preRow1[N - 1] - preRow1[i];
         const bottom = i > 0? preRow2[i - 1] : 0;
         const secondRobot = Math.max(top, bottom);
-        res = Math.min(res, secondRobot)
+        res = Math.min(res, secondRobot);
     }
 
-    return res;
+    return res; 
     
 };
