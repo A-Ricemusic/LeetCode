b236a399512a03985dc5f8427548dde61a995c95
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-08-31 15:34:41 -0700

    LC-reach-end-of-array-with-max-score [Runtime: N/A; Memory: N/A]

--- array/reach-end-of-array-with-max-score.js
+++ array/reach-end-of-array-with-max-score.js
@@ -4,19 +4,19 @@
  */
 var findMaximumScore = function(nums) {
     const n = nums.length;
-    const dp = new Array(n).fill(0);
+
     let max = nums[0];
     let j = 0;
     res = 0
     for (let i = 1; i < n - 1; i++) {
         if (nums[i] > max) {
-            res += (i - j) * max + dp[j]
+            res += (i - j) * max 
             j = i
             max = nums[i]
         }
     }
 
-    res += (n - 1 - j) * max + dp[j]
+    res += (n - 1 - j) * max 
     
 
     return res
