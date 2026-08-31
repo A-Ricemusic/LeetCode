aff7ca4f86489ff8785aa0f4afea00e73822b1a6
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-08-31 15:04:41 -0700

    LC-airplane-seat-assignment-probability [Runtime: 0 ms; Memory: 53.7 MB]

--- math/airplane-seat-assignment-probability.js
+++ math/airplane-seat-assignment-probability.js
@@ -3,11 +3,5 @@
  * @return {number}
  */
 var nthPersonGetsNthSeat = function(n) {
-    const dp = new Array(n + 1).fill(1)
-    for (let i = 2; i <= n; i++) {
-        dp[i] = (1 / i) * dp[i - 1]
-    }
-    return dp[n]
-
-    
+    return n === 1 ? 1 : 0.5;
 };
