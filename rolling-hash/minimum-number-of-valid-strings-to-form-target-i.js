a7567807257e684a9a5160c63c47a841a664539f
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-08-31 17:45:18 -0700

    LC-minimum-number-of-valid-strings-to-form-target-i [Runtime: N/A; Memory: N/A]

--- rolling-hash/minimum-number-of-valid-strings-to-form-target-i.js
+++ rolling-hash/minimum-number-of-valid-strings-to-form-target-i.js
@@ -3,24 +3,49 @@
  * @param {string} target
  * @return {number}
  */
-var minValidStrings = function(words, target) {
-    let count = 0;
-    let i = 0;
-    
-    while (i < target.length) {
-        let bestLen = 0;
-        for (let w of words) {
-            let len = 0;
-            while (i + len < target.length && 
-                   len < w.length && 
-                   target[i + len] === w[len]) {
-                len++;
-            }
-            bestLen = Math.max(bestLen, len);
-        }
-        if (bestLen === 0) return -1;
-        i += bestLen;
-        count++;
+
+ class TrieNode {
+    constructor() {
+        this.children = new Map();
     }
-    return count;
+ }
+
+class Trie {
+    constructor() {
+        this.root = new TrieNode()
+    }
+
+    insertWord(word) {
+        let curr = this.root
+        for (const c of word) {
+            if (!curr.children.has(c)) {
+                curr.children.set(c, new TrieNode())
+            }
+            curr = curr.children.get(c)
+        }
+    }
+}
+
+
+var minValidStrings = function(words, target) {
+    const trie = new Trie()
+    for (const word of words) {
+        trie.insertWord(word)
+    }
+
+    const n = target.length;
+    const dp = new Array(n + 1).fill(Infinity);
+    dp[n] = 0;
+
+    for (let i = n - 1; i >= 0; i--) {
+        let node = trie.root;
+        let j = i;
+        while (j < n && node.children.has(target[j])) {
+            node = node.children.get(target[j]);
+            j++;
+            dp[i] = Math.min(dp[i],1 + dp[j]);
+        }
+    }
+    
+    return dp[0] === Infinity? -1 : dp[0]
 };
