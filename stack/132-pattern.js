fff1931e0e9adb6294f3de69225915f24782e7de
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-07 13:51:46 -0700

    LC-132-pattern [Runtime: 12 ms; Memory: 73 MB]

--- stack/132-pattern.js
+++ stack/132-pattern.js
@@ -1,19 +1,23 @@
 /**
  * @param {number[]} nums
  * @return {boolean}
+ [3,5,0,3,4]
+ currMin = 3
+ [[5,3]]
+ 
+
  */
 var find132pattern = function(nums) {
-    let stack = []
-    for (let i = 0; i < nums.length; i++) {
-        const ref = stack.at(-1)
-        while (stack.length !== 0 && nums[i] < ref) {
-            if (nums[i] > stack.at(-1)) return true;
-            stack.pop();
+    let stack = [];
+    let currMin = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        while (stack.length !== 0 && stack.at(-1)[0] <= nums[i]) {
+            stack.pop()
         }
-
-        stack.push(nums[i])
+        if (stack.length !== 0 && stack.at(-1)[1] < nums[i]) return true;
+        stack.push([nums[i], currMin])
+        currMin = Math.min(currMin, nums[i]);
     }
-
     return false;
     
 };
