d9df98c19f59edf46846885eaeaabda29215cd5e
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-06 17:25:23 -0700

    LC-brick-wall [Runtime: N/A; Memory: N/A]

--- array/brick-wall.js
+++ array/brick-wall.js
@@ -8,15 +8,12 @@
 var leastBricks = function(wall) {
     const hashMap = new Map();
     const len = wall[0].reduce((a,c) => a + c, 0);
-    for (let i = 1; i < len; i++) {
-        hashMap.set(i,0);
-    }
 
     for (const row of wall) {
         let gap = 0;
         for (let i = 0; i < row.length - 1; i++) {
             gap += row[i];
-            hashMap.set(gap, hashMap.get(gap) + 1);
+            hashMap.set(gap, (hashMap.get(gap) || 0) + 1);
         }
     }
     let maxGap = 0
