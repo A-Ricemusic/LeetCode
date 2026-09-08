310bd7e9a3800a624fd4a5cbed3ca59f92e8dd04
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-07 21:40:21 -0700

    LC-number-of-ways-to-split-array [Runtime: N/A; Memory: N/A]

--- array/number-of-ways-to-split-array.js
+++ array/number-of-ways-to-split-array.js
@@ -2,23 +2,21 @@
  * @param {number[]} nums
  * @return {number}
 
- nums = [10,4,-8,7]
- n = 4
- i = 0
+time: o(n)
+space: o(n)
  */
 var waysToSplitArray = function(nums) {
     const n = nums.length 
     const total = nums.reduce((a,c) => a + c, 0);
-    const dfs = (i, sum) => {
-        if (i >= n - 1) return 0;
-        const left = sum + nums[i]
-        const right = total - left;
-        const count = left >= right? 1 : 0;
-        const res = dfs(i + 1, sum + nums[i]) + count;
-        return res;
-    };
-
-    return dfs(0,0)
-
+    let sum = 0;
+    let res = 0;
+    for (let i = 0; i < nums.length - 1; i++) {
+        sum += nums[i];
+        right = total - sum;
+        if (sum >= right) {
+            res++;
+        }
+    }
+    return res;
     
 };
