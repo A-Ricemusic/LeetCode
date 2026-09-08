aac9177900752a5972a9cbe02af24cc866b21a28
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-08 13:59:45 -0700

    LC-paths-in-maze-that-lead-to-same-room [Runtime: N/A; Memory: N/A]

--- graph/paths-in-maze-that-lead-to-same-room.js
+++ graph/paths-in-maze-that-lead-to-same-room.js
@@ -4,31 +4,24 @@
  * @return {number}
  time: o(n)
  space: o(n)
- dfs(1,1,0,[1]) => dfs(1,2,1,[])
  */
 var numberOfPaths = function(n, corridors) {
-    const adj = Array.from({length: n + 1}, () => []);
-    const visited = new Set();
+    const adj = Array.from({length: n + 1}, () => new Set());
     for (const [x,y] of corridors) {
-        adj[x].push(y);
-        adj[y].push(x);
+        adj[x].add(y);
+        adj[y].add(x);
     };
 
-    const dfs = (start, curr,len,path) => {
-        if (start === curr && len === 3) {
-            const key = [...path].sort((a,b) => a - b).join(",")
-            if (!visited.has(key)) {
-                visited.add(key);
-                return 1;
-            }
-            return 0;
+    const dfs = (start, curr,len) => {
+        if (len === 2) {
+            return adj[curr].has(start)? 1 : 0
         }
-        if (len >= 3) return 0;
+        
         let res = 0;
         for (const child of adj[curr]) {
-            path.push(curr);
-            res += dfs(start, child, len + 1,path);
-            path.pop()
+            if (child <= curr) continue;
+            res += dfs(start, child, len + 1);
+
         }
         return res;
     };
@@ -36,10 +29,7 @@ var numberOfPaths = function(n, corridors) {
 
     let final = 0;
     for (let i = 1; i <=n; i++) {
-        final += dfs(i,i,0,[])
+        final += dfs(i,i,0)
     }
-
     return final;
-
-    
 };
