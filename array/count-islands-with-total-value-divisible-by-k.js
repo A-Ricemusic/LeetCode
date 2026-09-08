0c51f08ebca20d5c9c05320630adf2a1b9f8253a
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-08 15:46:46 -0700

    LC-count-islands-with-total-value-divisible-by-k [Runtime: N/A; Memory: N/A]

--- array/count-islands-with-total-value-divisible-by-k.js
+++ array/count-islands-with-total-value-divisible-by-k.js
@@ -2,19 +2,19 @@
  * @param {number[][]} grid
  * @param {number} k
  * @return {number}
+ time: o(n * m)
+ space: o(n * m)
  */
 var countIslands = function(grid, k) {
-    const visited = new Set();
     const rows = grid.length;
     const cols = grid[0].length;
     const dirs = [[1,0],[0,1],[-1,0],[0,-1]];
 
 
     const dfs = (r,c) => {
-        const key = `${r},${c}`;
-        if (r < 0 || r >= rows || c < 0 || c >= cols || visited.has(key) || grid[r][c] === 0) return 0;
-        visited.add(key);
+        if (r < 0 || r >= rows || c < 0 || c >= cols || grid[r][c] === 0) return 0;
         let amt  = grid[r][c];
+        grid[r][c] = 0;
         for (const [dr,dc] of dirs) {
             amt += dfs(r + dr, c + dc)
         }
@@ -29,7 +29,7 @@ var countIslands = function(grid, k) {
 
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < cols; c++) {
-            if (grid[r][c] !== 0 && !visited.has(`${r},${c}`)) {
+            if (grid[r][c] !== 0) {
                 const amt = dfs(r,c)
                 if (amt % k === 0) {
                     res++;
