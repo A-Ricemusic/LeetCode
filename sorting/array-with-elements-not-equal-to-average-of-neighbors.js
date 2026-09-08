099b05407d5143009f196a5be72d52b702d19165
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-07 20:25:37 -0700

    LC-array-with-elements-not-equal-to-average-of-neighbors [Runtime: N/A; Memory: N/A]

--- sorting/array-with-elements-not-equal-to-average-of-neighbors.js
+++ sorting/array-with-elements-not-equal-to-average-of-neighbors.js
@@ -1,8 +1,32 @@
 /**
  * @param {number[]} nums
  * @return {number[]} 
-    nums = [1,2,3,4,2]
+    nums = [1,2,3,4,5]
  */
 var rearrangeArray = function(nums) {
-    return nums.sort((a,b) => a - b);
+    nums.sort((a,b) => a - b);
+    const len = Math.floor(nums.length / 2);
+    res = new Array(nums.length).fill(0);
+    const arr1 = [];
+    const arr2 = [];
+    for (let i = 0; i <= len; i++) {
+        arr1.push(nums[i])
+    }
+    for (let i = len + 1; i < nums.length; i++) {
+        arr2.push(nums[i])
+    }
+    let j = 0;
+    for (let i = 0; i < nums.length; i+=2) {
+        res[i] = arr1[j];
+        j++;
+    }
+
+    j = 0;
+    for (let i = 1; i < nums.length; i+=2) {
+        res[i] = arr2[j];
+        j++;
+    }
+    
+    return res;
+
 };
