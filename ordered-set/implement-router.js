1531d40e59d5d2713237535c1ced8e540030c6f4
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-08 16:45:12 -0700

    LC-implement-router [Runtime: N/A; Memory: N/A]

--- ordered-set/implement-router.js
+++ ordered-set/implement-router.js
@@ -1,12 +1,39 @@
 /**
  * @param {number} memoryLimit
+ n = number of packets
+ space: o(n)
  */
 var Router = function(memoryLimit) {
     this.cap = memoryLimit;
     this.q = [];
     this.head = 0;
     this.packets = new Set();
+    this.hashMap = new Map()
     
+};
+
+
+/**
+ * Removes the oldest packet from the router.
+ * Used by both eviction and forwarding.
+ */
+Router.prototype.removeOldest = function() {
+    if (this.head >= this.q.length) {
+        return null;
+    }
+
+    const packet = this.q[this.head++];
+    const [source, destination, timestamp] = packet;
+
+    // Remove from duplicate detection
+    const key = `${source},${destination},${timestamp}`;
+    this.packets.delete(key);
+
+    // Logically remove its timestamp from destination
+    const data = this.hashMap.get(destination);
+    data.head++;
+
+    return packet;
 };
 
 /** 
@@ -14,29 +41,45 @@ var Router = function(memoryLimit) {
  * @param {number} destination 
  * @param {number} timestamp
  * @return {boolean}
+ time: o(1)
  */
 Router.prototype.addPacket = function(source, destination, timestamp) {
-    const key2 = `${source},${destination},${timestamp}`;
-    if (this.packets.has(key2)) return false;
-    this.q.push([source, destination, timestamp]);
-    this.packets.add(key2);
-    if (this.q.length - this.head > this.cap) {
-        const arr = this.q[this.head++];
-        const key = `${arr[0]},${arr[1]},${arr[2]}`
-        this.packets.delete(key);
+    const key = `${source},${destination},${timestamp}`;
+
+    if (this.packets.has(key)) {
+        return false;
     }
-    return true; 
+
+    this.q.push([source, destination, timestamp]);
+    this.packets.add(key);
+
+    if (!this.hashMap.has(destination)) {
+        this.hashMap.set(destination, {
+            times: [],
+            head: 0
+        });
+    }
+
+    this.hashMap.get(destination).times.push(timestamp);
+
+    // Too many active packets
+    if (this.q.length - this.head > this.cap) {
+        this.removeOldest();
+    }
+
+    return true;
 };
 
 /**
  * @return {number[]}
+ time: o(1)
  */
 Router.prototype.forwardPacket = function() {
-    if (this.q.length - this.head === 0) return [];
-    const arr = this.q[this.head++];
-    const key = `${arr[0]},${arr[1]},${arr[2]}`
-    this.packets.delete(key);
-    return arr; 
+    if (this.head >= this.q.length) {
+        return [];
+    }
+
+    return this.removeOldest();
 };
 
 /** 
@@ -44,17 +87,58 @@ Router.prototype.forwardPacket = function() {
  * @param {number} startTime 
  * @param {number} endTime
  * @return {number}
+ time: o(log(n))
  */
 Router.prototype.getCount = function(destination, startTime, endTime) {
-    let res = 0;
-    for (let i = this.head; i < this.q.length; i++) {
-        const [source, des, timestamp] = this.q[i];
-        if (des === destination && startTime <= timestamp && timestamp <= endTime) {
-            res++;
-        }
+    if (!this.hashMap.has(destination)) {
+        return 0;
     }
 
-    return res;
+    const data = this.hashMap.get(destination);
+    const arr = data.times;
+    const start = data.head;
+
+    // Find first index where arr[index] >= startTime
+    const lowerBound = (target) => {
+        let l = start;
+        let r = arr.length;
+
+        while (l < r) {
+            const m = Math.floor((l + r) / 2);
+
+            if (arr[m] >= target) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        return l;
+    };
+
+    // Find first index where arr[index] > endTime
+    const upperBound = (target) => {
+        let l = start;
+        let r = arr.length;
+
+        while (l < r) {
+            const m = Math.floor((l + r) / 2);
+
+            if (arr[m] > target) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        return l;
+    };
+
+    const left = lowerBound(startTime);
+    const right = upperBound(endTime);
+
+    return right - left;
+   
 };
 
 /** 
