04f276d2f9c17a1a3f62cae7de616cc0f746017e
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-07 20:36:03 -0700

    LC-array-with-elements-not-equal-to-average-of-neighbors [Runtime: N/A; Memory: N/A]

--- greedy/array-with-elements-not-equal-to-average-of-neighbors.js
+++ greedy/array-with-elements-not-equal-to-average-of-neighbors.js
@@ -8,8 +8,9 @@
  */
 var rearrangeArray = function(nums) {
     nums.sort((a, b) => a - b);
+
     for (let i = 1; i < nums.length - 1; i += 2) {
-        [nums[i], nums[i + 1]] = [[nums[i + 1], nums[i]]];
+        [nums[i], nums[i + 1]] = [nums[i + 1], nums[i]];
     }
 
     return nums;
