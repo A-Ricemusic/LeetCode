70750414992bea9361435be00265c7fb747906c7
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-09 10:22:58 -0700

    LC-paths-in-maze-that-lead-to-same-room [Runtime: N/A; Memory: N/A]

--- graph/paths-in-maze-that-lead-to-same-room.js
+++ graph/paths-in-maze-that-lead-to-same-room.js
@@ -2,34 +2,34 @@
  * @param {number} n
  * @param {number[][]} corridors
  * @return {number}
- time: o(n)
- space: o(n)
  */
 var numberOfPaths = function(n, corridors) {
     const adj = Array.from({length: n + 1}, () => new Set());
     for (const [x,y] of corridors) {
         adj[x].add(y);
         adj[y].add(x);
-    };
+    }
 
-    const dfs = (start, curr,len) => {
+
+    const dfs = (start,curr,len) => {
         if (len === 2) {
-            return adj[curr].has(start)? 1 : 0
+            return adj[curr].has(start)? 1 : 0;
         }
-        
         let res = 0;
         for (const child of adj[curr]) {
             if (child <= curr) continue;
-            res += dfs(start, child, len + 1);
-
+            res += dfs(start,child,len + 1);
         }
+
         return res;
-    };
-
-
-    let final = 0;
-    for (let i = 1; i <=n; i++) {
-        final += dfs(i,i,0)
     }
+
+
+
+    let final = 0
+    for (let i = 1; i <=n; i++) {
+        final += dfs(i,i,len)
+    }
+
     return final;
 };
