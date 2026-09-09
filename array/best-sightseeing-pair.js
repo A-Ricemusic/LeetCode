89a8d1a941e845522a1dd98657c012a862fe258f
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-08 21:22:32 -0700

    LC-best-sightseeing-pair [Runtime: 1 ms; Memory: 60.5 MB]

--- array/best-sightseeing-pair.js
+++ array/best-sightseeing-pair.js
@@ -1,16 +1,14 @@
 /**
  * @param {number[]} values
  * @return {number}
-  values = [8,10,5,2,20]
-  
  */
 var maxScoreSightseeingPair = function(values) {
+    let val1Max = values[0];
     let res = 0;
-    for (let i = 0; i < values.length - 1; i++) {
-        for (let j = i + 1; j < values.length; j++) {
-            const sum = values[i] + values[j] + (i - j);
-            res = Math.max(res, sum)
-        }
+    for (let i = 1; i < values.length; i++) {
+        let value2 = (values[i] - i);
+        res = Math.max(res, val1Max + value2);
+        val1Max = Math.max(val1Max, values[i] + i)
     }
     return res;
 };
