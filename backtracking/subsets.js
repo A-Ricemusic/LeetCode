3cc4be9851def1dbf6a0755ee2bd8b7d55b3d3ed
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-11 16:32:33 -0700

    LC-subsets [Runtime: N/A; Memory: N/A]

---
+++ backtracking/subsets.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+    const res = [];
+    const curr = [];
+
+    const dfs = (i) => {
+        if (i >= nums.length) {
+            res.push([...curr])
+        }
+        dfs(i + 1)
+        curr.push(nums[i])
+        dfs(i + 1)
+        curr.pop()
+    }
+
+    dfs(0)
+    return res;
+    
+};
