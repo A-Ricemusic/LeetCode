00b4be5301834677b65c1670faf43130214995ef
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-11 16:20:47 -0700

    LC-target-sum [Runtime: N/A; Memory: N/A]

--- array/target-sum.js
+++ array/target-sum.js
@@ -2,21 +2,33 @@
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
+ n = length of nums
+ m = amount of sum combinations
+     0  1. 2. 3. 4 5 6 7 8 9 10
+    -5,-4,-3,-2,-1,0,1,2,3,4,5
+  0 [0,0,  0, 0, 1,0,1,0,0,0,0]
+  1 [0,0,  0, 1, 0,2,0,1,0,0,0]
+  2 [0,0,  1, 0, 3,0,3,0,1,0,0]
+  3 [0,1,  0, 3, 0,6,0,4,0,1,0]
+  4 [1,0,  4, 0, 9,0,10,0,5,0,1]
  */
 var findTargetSumWays = function(nums, target) {
-    const memo= new Map();
-    const dfs = (i, sum) => {
-        if (i >= nums.length) {
-            return sum === target? 1 : 0
+    const sum = nums.reduce((a,c) => a + c, 0)
+    const totalSum = (sum * 2) + 1;
+    const dp = Array.from({length: nums.length}, () => new Array(totalSum).fill(0));
+    dp[0][sum + nums[0]] = 1;
+    dp[0][sum - nums[0]] = 1;
+    for (let i = 1; i < nums.length; i++) {
+        for (let j = 0; j < totalSum; j++) {
+            if (j === 0) {
+                dp[i][j] = dp[i - 1][j + 1];
+            } else if (j === totalSum - 1) {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1];
+            }
         }
-        const state = `${i},${sum}`;
-        if (memo.has(state)) return memo.get(state);
-        const c1 = dfs(i + 1, sum + nums[i])
-        const c2 = dfs(i + 1, sum - nums[i])
-        const res = c1 + c2;
-        memo.set(state, res);
-        return res;
-    };
+    }
 
-    return dfs(0,0);
+    return dp[nums.length - 1][sum + target]
 };
