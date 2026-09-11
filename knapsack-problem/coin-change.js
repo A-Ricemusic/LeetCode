f39b7b999dced3eef823995ae102a9efe101e204
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-10 17:17:40 -0700

    LC-coin-change [Runtime: N/A; Memory: N/A]

--- knapsack-problem/coin-change.js
+++ knapsack-problem/coin-change.js
@@ -8,9 +8,12 @@
  */
 var coinChange = function(coins, amount) {
     const q = [[amount, 0]];
+    const visited = new Set()
     let head = 0;
     while (q.length - head > 0) {
         const [amt, numOfCoins] = q[head++];
+        if (visited.has(amt)) continue;
+        visited.add(amt);
         if (amt === 0) return numOfCoins;
         if (amt < 0) continue;
         for (const coin of coins) {
