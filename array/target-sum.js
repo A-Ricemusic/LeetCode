35e957ae3c1916a8920cbfab5dde74c3dc9c2dd8
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-11 16:29:56 -0700

    LC-target-sum [Runtime: N/A; Memory: N/A]

--- array/target-sum.js
+++ array/target-sum.js
@@ -15,20 +15,22 @@
 var findTargetSumWays = function(nums, target) {
     const sum = nums.reduce((a,c) => a + c, 0)
     const totalSum = (sum * 2) + 1;
+    if (Math.abs(target) > sum) return 0;
     const dp = Array.from({length: nums.length}, () => new Array(totalSum).fill(0));
-    dp[0][sum + nums[0]] = 1;
-    dp[0][sum - nums[0]] = 1;
+    dp[0][sum + nums[0]] += 1;
+    dp[0][sum - nums[0]] += 1;
     for (let i = 1; i < nums.length; i++) {
         for (let j = 0; j < totalSum; j++) {
-            if (j === 0) {
-                dp[i][j] = dp[i - 1][j + nums[i]];
-            } else if (j === totalSum - 1) {
-                dp[i][j] = dp[i - 1][j - nums[i]];
-            } else {
-                dp[i][j] = dp[i - 1][j - nums[i]] + dp[i - 1][j + nums[i]];
+            const left = j - nums[i];
+            const right = j + nums[i]
+            if (left >= 0) {
+                dp[i][j] += dp[i - 1][left]
+            }
+            if (right < totalSum) {
+                dp[i][j] += dp[i - 1][right]
             }
         }
     }
 
-    return sum + target >= totalSum? 0 : dp[nums.length - 1][sum + target]
+    return  dp[nums.length - 1][sum + target]
 };
