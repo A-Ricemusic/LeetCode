040aea23dfc13622de4b6e6e534b457ab261f0ed
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-10 17:15:14 -0700

    LC-coin-change [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/coin-change.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ amount = 11 
+ [10,1]
+  [[9,1], [6,1], [9,2], [8,2], [5,2]]
+ */
+var coinChange = function(coins, amount) {
+    const q = [[amount, 0]];
+    let head = 0;
+    while (q.length - head > 0) {
+        const [amt, numOfCoins] = q[head++];
+        if (amt === 0) return numOfCoins;
+        if (amt < 0) continue;
+        for (const coin of coins) {
+            q.push([amt - coin,numOfCoins + 1])
+        }
+    }
+
+    return -1
+
+    
+};
