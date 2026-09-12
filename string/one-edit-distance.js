96392b28803cdd9389cf11b24e45c3ce54a00d80
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-11 19:57:09 -0700

    LC-one-edit-distance [Runtime: N/A; Memory: N/A]

--- string/one-edit-distance.js
+++ string/one-edit-distance.js
@@ -4,8 +4,8 @@
  * @return {boolean}
  i = 1
  j = 1
- s = "a"
- t = "ab"
+ s = "ab"
+ t = "acb"
  if char are same i + 1, j + 1
   dfs(i + 1, j), dfs(i,j + 1), dfs(i + 1,j + 1)
 
@@ -14,7 +14,7 @@ var isOneEditDistance = function(s, t) {
     const memo = new Map()
     const n = s.length;
     const m = t.length;
-    const dfs = (i,j) => {
+    const dfs = (i,j,curr) => {
         if (i >= n) {
             return m - j;
         }
@@ -23,16 +23,20 @@ var isOneEditDistance = function(s, t) {
         }
         const state = `${i},${j}`;
         if (memo.has(state)) return memo.get(state);
+        if (curr > 1) {
+            memo.set(state, Infinity)
+            return Infinity;
+        }
         let res
         if (s[i] === t[j]) {
-            res = dfs(i + 1,j + 1)
+            res = dfs(i + 1,j + 1, curr) 
         } else {
-            res = Math.min(dfs(i + 1,j), dfs(i,j + 1), dfs(i + 1,j + 1)) + 1
+            res = Math.min(dfs(i + 1,j,curr + 1), dfs(i,j + 1,curr + 1), dfs(i + 1,j + 1,curr + 1)) + 1
         }
         memo.set(state, res)
         return res;
     }
 
-    return dfs(0,0) === 1
+    return dfs(0,0,0) === 1
     
 };
