6dfa362a45b88826d3b76d01b75e885d7ec39ad7
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-11 20:10:41 -0700

    LC-one-edit-distance [Runtime: 58 ms; Memory: 60.1 MB]

--- two-pointers/one-edit-distance.js
+++ two-pointers/one-edit-distance.js
@@ -10,23 +10,24 @@
   dfs(i + 1, j), dfs(i,j + 1), dfs(i + 1,j + 1)
 
  */
-var isOneEditDistance = function(s, t,curr) {
+var isOneEditDistance = function(s, t) {
     const memo = new Map()
     const n = s.length;
     const m = t.length;
-    const dfs = (i,j) => {
-        if (i >= n) {
-            return m - j === 1;
-        }
-        if (j >= m) {
-            return n - i === 1;
-        }
-        const state = `${i},${j}`;
-        if (memo.has(state)) return memo.get(state);
+    const dfs = (i,j,curr) => {
+        const state = `${i},${j},${curr}`;
+        
         if (curr > 1) {
             memo.set(state, false);
             return false;
         }
+        if (i >= n) {
+            return (m - j) + curr === 1;
+        }
+        if (j >= m) {
+            return (n - i) + curr === 1;
+        }
+        if (memo.has(state)) return memo.get(state);
         let res
         if (s[i] === t[j]) {
             res = dfs(i + 1,j + 1,curr)
