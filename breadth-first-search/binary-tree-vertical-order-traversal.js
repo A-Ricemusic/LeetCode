070c14089f90ea1290e532701e339ebc6bd20c2e
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-11 18:23:02 -0700

    LC-binary-tree-vertical-order-traversal [Runtime: N/A; Memory: N/A]

---
+++ breadth-first-search/binary-tree-vertical-order-traversal.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ hashMap = {
+ 0: [3,0,1]
+ -1: [9]
+ -2: [4]
+ 1: [8]
+ 2: [7]
+ }
+ 
+ */
+var verticalOrder = function(root) {
+    const hashMap = new Map()
+
+    const preorder = (curr, pos) => {
+        if (!curr) return;
+        if (!hashMap.has(pos)) {
+            hashMap.set(pos, [])
+        }
+        hashMap.get(pos).push(curr.val)
+        preorder(curr.left, pos - 1)
+        preorder(curr.right, pos + 1)
+    }
+
+    dfs(root,0)
+    const entries = [..hashMap.entries()].sort((a,b) => a[0] - b[0]);
+
+    const res = []
+    for (const [k,v] of entries) {
+        res.push(v)
+    }
+
+    return res;
+};
