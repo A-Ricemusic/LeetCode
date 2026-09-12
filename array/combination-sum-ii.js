fb8c821203ec0f8fb21845a335297f6681f1602e
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-11 19:08:35 -0700

    LC-combination-sum-ii [Runtime: N/A; Memory: N/A]

--- array/combination-sum-ii.js
+++ array/combination-sum-ii.js
@@ -2,27 +2,39 @@
  * @param {number[]} candidates
  * @param {number} target
  * @return {number[][]}
+
+ candidates = [10,1,2,7,6,1,5]
+ [1,1,1,5,6,7,10]
+ [2,5,2,1,2]
+ [1,2,2,2,5]
+
+ dfs(0,0) => dfs(1,0), dfs(1,1)
+ dfs(1,0) => dfs(2,0)
  */
 var combinationSum2 = function(candidates, target) {
-    const res = []
-    const path = []
-    const nums = candidates
+    const res = [];
+    const path = [];
+    const nums = candidates;
+    nums.sort((a,b) => a - b);
 
     const dfs = (i, sum) => {
         if (sum === target) {
             res.push([...path]);
             return
         }
-        if (i >= nums.length) return
+        if (i >= nums.length) return;
+        path.push(nums[i]);
+        dfs(i + 1, sum + nums[i]);
+        path.pop(); 
+        let idx = i + 1;
+        while (idx < nums.length && nums[idx] === nums[idx - 1]) {
+            idx++;
+        }
         
-        dfs(i + 1, sum)
-        path.push(nums[i])
-        dfs(i + 1,sum + nums[i])
-        path.pop()
+        dfs(idx, sum);
     }
 
     dfs(0, 0)
-
     return res;
     
 };
