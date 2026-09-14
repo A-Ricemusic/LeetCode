032f4bfb08cdd9d5af50bf3ce167faa83e4f0565
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-14 15:48:28 -0700

    LC-merge-k-sorted-lists [Runtime: N/A; Memory: N/A]

---
+++ divide-and-conquer/merge-k-sorted-lists.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ dummy -> 1 -> 
+
+ [, [1,node], [2,node], [4,node]]
+ */
+var mergeKLists = function(lists) {
+    const minHeap = new PriorityQueue((a,b) => a[0] - b[0]);
+    const dummy = new ListNode(0)
+    for (const l of list) {
+        minHeap.enqueue([l.val,l])
+    }
+    let curr = dummy;
+    while (!minHeap.isEmpty()) {
+        const [_, node] = minHeap.dequeue();
+        curr.next = node;
+        curr = curr.next
+        if (node.next) {
+            minHeap.enqueue([node.val,node])
+        }
+    }
+
+    return dummy.next;
+    
+};
