ef4ae52474c4b5b5c7f108dcff316933b17d023f
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-15 17:39:22 -0700

    LC-redundant-connection [Runtime: N/A; Memory: N/A]

---
+++ union-find/redundant-connection.js
@@ -0,0 +1,48 @@
+/**
+ * @param {number[][]} edges
+ * @return {number[]}
+ */
+var findRedundantConnection = function(edges) {
+    const n = edges.length;
+    const par = new Array(n + 1).fill(0);
+    const rank = new Array(n + 1).fill(0);
+    for (let i = 1; i <= n; i++) {
+        par[i] = i;
+    }
+
+
+    const find = (x) => {
+        let par = par[x];
+        while (par[x] !== par) {
+            par[x] = par[par[x]]
+            par = par[x];
+        }
+
+        return par;
+    }
+
+
+    const union = (x,y) => {
+        const p1 = find(x);
+        const p2 = find(y);
+        if (p1 === p2) return false;
+        if (rank[p1] < rank[p2]) {
+            par[p1] = p2; 
+        } else if (rank[p2] < rank[p1])  {
+            par[p2] = p1
+        } else {
+            par[p1] = p2;
+            rank[p2]++;
+        }
+
+        return true;
+    }
+
+    for (const [x,y] of edges) {
+        if (!union(x,y)) {
+            return [x,y]
+        }
+    }
+
+    return []
+};
