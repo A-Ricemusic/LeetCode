2574d725d8201d36a31c84f90369308d70f625bf
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-15 17:21:07 -0700

    LC-combinations [Runtime: N/A; Memory: N/A]

---
+++ backtracking/combinations.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+
+ time: O(k * 2^n)
+ space: O(n)
+ */
+var combine = function(n, k) {
+    const nums = [];
+    let res = [];
+    for (let i = 1; i <= n; i++) {
+        nums.push(i);
+    }
+
+    const dfs = (i, path) => {
+        if (path.length === k) {
+            res.push([...path]) 
+            return;
+        }
+        if (i >= n) return;
+        dfs(i + 1, path);
+        path.push(nums[i])
+        dfs(i + 1, path)
+        path.pop()
+
+    }
+
+
+    dfs(0,[]);
+    return res;
+    
+};
