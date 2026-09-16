2c5f8734a9a8f57824f47e1d7543c582b7602f0f
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-16 16:45:03 -0700

    LC-min-cost-to-connect-all-points [Runtime: N/A; Memory: N/A]

--- prims-algorithm/min-cost-to-connect-all-points.js
+++ prims-algorithm/min-cost-to-connect-all-points.js
@@ -3,21 +3,23 @@
  * @return {number}
  */
 var minCostConnectPoints = function(points) {
-    const minHeap = new PriorityQueue((a,b) => a[0] - b[0]);
     const visited = new Set();
     const n = points.length;
-    minHeap.enqueue([0,0]);
+    const minDist = new Array(n).fill(Infinity);
     let res = 0;
+    minDist[0] = 0;
 
     while (visited.size < n) {
-        const [cost,pointIdx] = minHeap.dequeue();
-        if (visited.has(pointIdx)) continue;
-        visited.add(pointIdx);
-        res += cost;
+        let currNode = -1;
+        const min = Math.min(...minDist)
+        currNode = minDist.findIndex(num => num === min);
+        res += min;
+        visited.add(currNode)
+        minDist[currNode] = Infinity;
         for (let i = 0; i < n; i++) {
             if (visited.has(i)) continue;
-            const dist = Math.abs(points[i][0] - points[pointIdx][0]) + Math.abs(points[i][1] - points[pointIdx][1]);
-            minHeap.enqueue([dist, i])
+            const dist = Math.abs(points[currNode][0] - points[i][0]) + Math.abs(points[currNode][1] - points[i][1]);
+            minDist[i] = Math.min(minDist[i], dist)
         }
     }
 
