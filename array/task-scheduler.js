fe18c369b1993e5cd31c236478547940f294d4e6
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-16 17:26:27 -0700

    LC-task-scheduler [Runtime: 56 ms; Memory: 67.1 MB]

--- array/task-scheduler.js
+++ array/task-scheduler.js
@@ -2,10 +2,12 @@
  * @param {character[]} tasks
  * @param {number} n
  * @return {number}
+
+ time: 
  */
 var leastInterval = function(tasks, n) {
     const hashMap = new Map();
-    const heap = new PriorityQueue((a,b) => a[0] === b[0]? b[1] - a[1] : a[0] - b[0]);
+    const heap = new PriorityQueue((a,b) => b[1] - a[1]);
     const q = [];
     let head = 0;
     let t = 0;
@@ -14,23 +16,22 @@ var leastInterval = function(tasks, n) {
     };
 
     for (const [k,v] of hashMap.entries()) {
-        q.push([0,v]);
+        heap.enqueue([0,v]);
     };
 
     while (q.length - head > 0 || !heap.isEmpty()) {
-        while (!heap.isEmpty() && t >= heap.front()[0]) {
-            const arr = heap.dequeue();
-            q.push(arr);
+        while (q.length - head > 0 && t > q[head][0]) {
+            const arr = q[head++];
+            heap.enqueue(arr)
         }
 
-        if (q.length - head === 0) {
+        if (heap.isEmpty()) {
             t++;
             continue;
         }
-
-        const [_,freq] = q[head++];
+        const [_,freq] = heap.dequeue()
         if (freq - 1 > 0) {
-            heap.enqueue([t + n + 1, freq - 1])
+            q.push([t + n, freq - 1]);
         }
         t++;
     }
