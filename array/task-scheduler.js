fddc9eddb19928229316462d6c83a52d97f628a3
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-16 17:32:19 -0700

    LC-task-scheduler [Runtime: N/A; Memory: N/A]

--- array/task-scheduler.js
+++ array/task-scheduler.js
@@ -3,22 +3,28 @@
  * @param {number} n
  * @return {number}
 
- time: 
+ m = number of task
+ n = wait time
+ k: distinct task which is at most 26
+ time: O(m + (n * m))
+ space: O(1)
  */
 var leastInterval = function(tasks, n) {
-    const hashMap = new Map();
-    const heap = new PriorityQueue((a,b) => b[1] - a[1]);
-    const q = [];
+    const hashMap = new Map(); //O(26)
+    const heap = new PriorityQueue((a,b) => b[1] - a[1]);// O(26) space
+    const q = []; //O(26) space
     let head = 0;
     let t = 0;
+    //time: O(m)
     for (const task of tasks) {
         hashMap.set(task, (hashMap.get(task) ?? 0) + 1);
     };
 
+    //time: O(klog(k)) => O(26log(26))
     for (const [k,v] of hashMap.entries()) {
         heap.enqueue([0,v]);
     };
-
+   // in the worst case: time: O(n * mlog(k)) => O(n * mlog(26)) => O(n * m)
     while (q.length - head > 0 || !heap.isEmpty()) {
         while (q.length - head > 0 && t > q[head][0]) {
             const arr = q[head++];
