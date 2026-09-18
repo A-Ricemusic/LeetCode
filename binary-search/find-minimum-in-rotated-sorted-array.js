8fc214681f4de79d4e02b8e28b224d986ccaa78d
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-17 18:08:11 -0700

    LC-find-minimum-in-rotated-sorted-array [Runtime: N/A; Memory: N/A]

--- binary-search/find-minimum-in-rotated-sorted-array.js
+++ binary-search/find-minimum-in-rotated-sorted-array.js
@@ -1,10 +1,24 @@
 /**
  * @param {number[]} nums
  * @return {number}
-    nums = [5,6,2,3,4]
+    nums = [5,6,7,8,2,3,4]
     nums = [3,4,5,6,1]
     nums = [1,4,5,6,7]
+
+     0 1 2 3 4 5 6
+    [5,6,7,8,9,3,4]
+
+
+    0 1 2 3 4 5 6
+    [5,6,7,8,9,10,11]
+
+    0 1 2 3 4 5 6
+    [7,8,2,3,4,5,6]
+
+
+     [10,6,7,2,4]
  */
+
 var findMin = function(nums) {
 
     let l = 0;
@@ -18,11 +32,18 @@ var findMin = function(nums) {
             return nums[m];
         }
 
-        if (num > nums[0]) {
-            l = m + 1;
-        } else {
-            r = m - 1;
+        const end = nums[nums.length - 1];
+        const start = nums[0];
+        if (nums[m] < end && nums[m] < start) {
+            r = m - 1
+        } else if (nums[m] < end && nums[m] > start) {
+            r = m - 1
+        } else if (nums[m] > start && nums[m] > end) {
+            l = m + 1
+        } else if (nums[m] > end && nums[m] < start) {
+            l = m + 1
         }
+
     }
 
 
