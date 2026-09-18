bb64af068b974aac33997e8c2abf0e7e6ee4ed9f
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-18 10:25:51 -0700

    LC-maximum-product-subarray [Runtime: N/A; Memory: N/A]

--- dynamic-programming/maximum-product-subarray.js
+++ dynamic-programming/maximum-product-subarray.js
@@ -2,29 +2,19 @@
  * @param {number[]} nums
  * @return {number}
 
- nums = [-2,0,2,2,4,6]
-
- curr = 8
- currMax = 8
- currMin = 4
- globalMax = 4
 
 
  */
 var maxProduct = function(nums) {
-    let curr = nums[0];
     let currMax = nums[0];
     let currMin = nums[0];
     let res = nums[0];
 
     for (let i = 1; i < nums.length; i++) {
-        curr *= nums[i]
-        currMax = Math.max(nums[i], curr);
-        currMin = Math.min(nums[i], curr);
+        const tmp = currMax
+        currMax = Math.max(nums[i], currMax * nums[i], currMin * nums[i]);
+        currMin = Math.min(nums[i], currMin * nums[i], tmp * nums[i]);
         res = Math.max(res, currMax);
-        if (curr === 0) {
-            curr = 1;
-        }
     }
 
     return res
