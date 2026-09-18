0da9d4f21f7723e3780959470c02ba76b05d7d74
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-17 17:24:08 -0700

    LC-reverse-nodes-in-k-group [Runtime: N/A; Memory: N/A]

---
+++ linked-list/reverse-nodes-in-k-group.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var reverseKGroup = function(head, k) {
+    let revCount = 0;
+    const dummy = new ListNode(0);
+    let prev = dummy;
+    let curr = head;
+    let last = head;
+    let count = 1;
+
+    while (curr.next) {
+        const next = curr.next;
+        if (count === k) {
+            reverseList(last, curr);
+            prev.next = curr;
+            prev = last;
+            revCount++;
+            count = 0;
+            last = next;
+        }
+        count++;
+        curr = next;
+    }
+    prev.next = last
+
+    return revCount >= 1? dummy.next : head;
+    
+};
+
+const reverseList = function(last, curr) {
+    let prev = null;
+    while (prev !== curr) {
+        const nxt = last.next
+        last.next = prev;
+        prev = last
+        last = nxt;
+    }
+}
